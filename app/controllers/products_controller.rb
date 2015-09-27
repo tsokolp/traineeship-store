39053157327f942dccd2ba6a97430dd9acd96907
falcont <tsokolp@gmail.com>
2015-09-27 10:23:24 +0000

    Add Product model

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,25 @@
+class ProductsController < ApplicationController
+    
+    def index
+    	@products = Product.all	
+    end
+    
+    def show
+    	@product = Product.find(params[:id])
+    end
+    
+    def new
+    end
+    
+   	def create
+  		@product = Product.new(product_params)
+ 
+  		@product.save
+  		redirect_to @product
+  	end
+  	
+  	private
+  			def product_params
+  				params.require(:product).permit(:title, :description)
+  			end
+end
