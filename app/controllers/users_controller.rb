d0d011c17e635a4686818a389ab0cb8c512aca33
falcont <tsokolp@gmail.com>
2015-09-27 12:54:08 +0000

    add user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 	
 	private
 		def user_params
-			params.requre(:user).permit(:name, :email, :password)
+			params.require(:user).permit(:name, :email, :password)
 		end
 		
 	
