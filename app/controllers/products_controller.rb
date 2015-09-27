c0b087445acda758214064e0bffab1f694daf05d
falcont <tsokolp@gmail.com>
2015-09-27 10:46:25 +0000

    add validations

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,13 +9,17 @@ class ProductsController < ApplicationController
     end
     
     def new
+    	@product = Product.new
     end
     
    	def create
   		@product = Product.new(product_params)
  
-  		@product.save
-  		redirect_to @product
+  		if @product.save
+  			redirect_to @product
+  		else
+  			render 'new'
+  		end
   	end
   	
   	private
