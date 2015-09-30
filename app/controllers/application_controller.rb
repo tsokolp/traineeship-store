59111b051b4e5e3c798de3512d3bbd984d3423e4
falcont <tsokolp@gmail.com>
2015-09-30 18:38:19 +0000

    some fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,11 @@ class ApplicationController < ActionController::Base
 
   def current_user 
     @current_user ||= User.find(session[:user_id]) if session[:user_id] 
+    rescue ActiveRecord::RecordNotFound
   end
   
   def require_user 
-    redirect_to '/login' unless current_user 
+    redirect_to login_path unless current_user 
   end
 
 end
