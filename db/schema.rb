5d38458a4baa5b31551108181ba907a6779b51aa
falcont <tsokolp@gmail.com>
2015-11-13 11:55:16 +0000

    part1

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150927123055) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "category"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
   end
@@ -28,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150927123055) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
+    t.string   "role"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
